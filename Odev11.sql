f21803f21e3d4d5703fd37fa80b171df62bb82e6
yigitcanga <105357336+yigitcanga@users.noreply.github.com>
2022-08-14 17:35:20 +0300

    Add files via upload

---
+++ Odev11.sql
@@ -0,0 +1,63 @@
+-- 1
+(
+SELECT first_name FROM actor  
+ORDER BY first_name
+)
+UNION
+(
+SELECT first_name FROM customer  
+ORDER BY first_name
+)
+
+-- 2
+(
+SELECT first_name FROM actor  
+ORDER BY first_name
+)
+INTERSECT
+(
+SELECT first_name FROM customer  
+ORDER BY first_name
+)
+
+-- 3
+(
+SELECT first_name FROM actor  
+ORDER BY first_name
+)
+EXCEPT
+(
+SELECT first_name FROM customer  
+ORDER BY first_name
+)
+
+-- 4
+(
+SELECT first_name FROM actor  
+ORDER BY first_name
+)
+UNION ALL
+(
+SELECT first_name FROM customer  
+ORDER BY first_name
+)
+
+(
+SELECT first_name FROM actor  
+ORDER BY first_name
+)
+INTERSECT
+(
+SELECT first_name FROM customer  
+ORDER BY first_name
+)
+
+(
+SELECT first_name FROM actor  
+ORDER BY first_name
+)
+EXCEPT ALL
+(
+SELECT first_name FROM customer  
+ORDER BY first_name
+)
